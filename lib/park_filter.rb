d9f2f4a94207879fd7d321175fa0f1b29d2aef8d
Shelby Kelly <shelby.kelly@colorado.edu>
2014-07-14 10:26:09 -0600

    Turning in assessment 3

--- lib/park_filter.rb
+++ lib/park_filter.rb
@@ -17,28 +17,12 @@ class ParkFilter
 
   def indexing_by_country
 
+    keys = @parks.map {|park_hash| park_hash[:country]}
 
+    zipped = keys.zip(@parks)
+    Hash[zipped].sort_by {|key, value| key}.to_h
 
-
-
-
-
-
-
-
-
-
-
-
-
-    # keys = @parks.map {|park_hash| park_hash[:country]}
-    #
-    # zipped = keys.zip(@parks)
-    # Hash[zipped]
-
-
-
-
+  #need to find second united states and put hashes in array
   end
 
 end
